de039935e045023d9381fa47589a28d67a3659b1
Sparker36 <Parkers@etsu.edu>
2020-09-12 21:14:27 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TicTacToeApp.Logic;
+
+namespace TicTacToeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeBoard sut = new TicTacToeBoard();
+            int row, col;
+            Console.WriteLine(sut.ToString());
+            var result = sut.ReportResult();
+            do
+            {
+                Console.WriteLine("Please insert the row to place an X: ");
+                row = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Please insert the col to place an X: ");
+                col = Int32.Parse(Console.ReadLine());
+                sut.InsertX(row, col);
+                Console.WriteLine(sut.ToString());
+
+                result = sut.ReportResult();
+
+                Console.WriteLine("Please insert the row to place an O: ");
+                row = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Please insert the col to place an O: ");
+                col = Int32.Parse(Console.ReadLine());
+                sut.InsertO(row, col);
+                Console.WriteLine(sut.ToString()); ;
+            } while (result == 'N');
+
+            switch (sut.ReportResult())
+            {
+                case 'X':
+                    Console.WriteLine("X wins the game!");
+                    break;
+                case 'O':
+                    Console.WriteLine("O wins the game!");
+                    break;
+                case 'D':
+                    Console.WriteLine("The game is a draw.");
+                    break;
+            }
+            Console.WriteLine(sut.ToString());
+            Console.ReadKey();
+        }
+    }
+}
